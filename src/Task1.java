6d1a0d1f2f4700745604ba2e4d305f20e6d6b9a2
DenSlizko <denis.slizko@gmail.com>
2016-03-16 19:24:44 +0200

    Clear code and make it shorter

--- src/Task1.java
+++ src/Task1.java
@@ -3,25 +3,25 @@ import java.util.Random;
 public class Task1 {
     public static void main(String[] args) {
 
-        int[] mas = new int[5];
+        int[] mas = new int[7];
         Random rand = new Random();
 
-        for (int i = 0; i < mas.length; i++)
-            mas[i] = rand.nextInt(100);
-
-        for (int i = 0; i < mas.length; i++)
-            System.out.println(mas[i] + " ");
-        int max = mas[0];
         for (int i = 0; i < mas.length; i++) {
-            if (max < mas[i])
-                max = mas[i];
+            mas[i] = rand.nextInt(100);
+            System.out.println(mas[i] + " ");
         }
-        System.out.println(" maximum = " + max);
+
+        int max = mas[0];
         int min = mas[0];
         for (int i = 0; i < mas.length; i++) {
-            if (min > mas[i])
+            if (max < mas[i]) {
+                max = mas[i];
+            }
+            if (min > mas[i]) {
                 min = mas[i];
+            }
         }
+        System.out.println(" maximum = " + max);
         System.out.println(" minimum = " + min);
     }
 }
